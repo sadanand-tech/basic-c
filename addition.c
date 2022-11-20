d2d8186cf8341b549abfd7cd853eb364b0893cf8
sadanand <sgkp2412@gmail.com>
2022-11-20 15:44:39 +0530

    first

---
+++ addition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter a number");
+    scanf("%d",&a);
+    if(a%2==0){
+
+    printf("%d no is even",a);
+    }else{
+         printf("%d no is odd",a);
+          }
+    return 0;
+
+}
